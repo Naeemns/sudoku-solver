db6040d97d824e79c3d92576576f2865ff54ceb4
Naeemns <naeemshaik894@gmail.com>
2021-04-18 20:07:17 +0530

    Change in sample input

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const message = document.querySelector(".message h1");
 
 // console.log(inputs[78].value == "")
 
-const sampleInput = `0 0 0 0 5 4 3 0 6
-0 0 0 0 0 3 2 7 0
-0 0 0 7 2 0 0 0 1
-9 0 0 0 7 0 0 5 3
-0 0 0 0 0 0 0 0 0
-8 2 0 0 1 0 0 0 9
-3 0 0 0 6 1 0 0 0
-0 4 6 9 0 0 0 0 0
-7 0 1 5 4 0 0 0 6`;
+const sampleInput = `0 4 0 0 0 0 1 7 9 
+0 0 2 0 0 8 0 5 4 
+0 0 6 0 0 5 0 0 8 
+0 8 0 0 7 0 9 1 0 
+0 5 0 0 9 0 0 3 0 
+0 1 9 0 6 0 0 4 0 
+3 0 0 4 0 0 7 0 0 
+5 7 0 1 0 0 2 0 0 
+9 2 8 0 0 0 0 6 0`;
 
 let possible = false;
 let resetSudoku = false;
